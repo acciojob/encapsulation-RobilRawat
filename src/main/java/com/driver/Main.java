f01089f637db53fd71b773b9d07cd941566b1bcf
RobilRawat <robil.cs.coder@gmail.com>
2023-06-28 10:38:05 +0530

    Added solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+       // obj.name="Robil";   name variable is a private member
+
+        obj.setName("Robil");
+        obj.getName();
+
+
+    }
   
 }
